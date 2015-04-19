e94aaaf91c091816d5a84830bb7c04edbcc6d5af
diwashJangam <diwash.jangam@mavs.uta.edu>
2015-04-19 11:09:23 -0500

    adding gapminder output

--- combine.sh
+++ combine.sh
@@ -7,6 +7,7 @@
 OUTPUT_FILE="output/combined_gapMinder.tsv"
 
 echo Building combined country data file
+# put files head to tail.
 cat data/clean/country.cc.txt > $OUTPUT_FILE
 
 mv -v data/clean/country.cc.txt data/clean/country.cc.processed
@@ -14,6 +15,7 @@ sleep 2
 
 cat data/clean/*.cc.txt 
 sleep 2
+#>> adds to previous.... > delets and adds
 cat data/clean/*.cc.txt >> $OUTPUT_FILE
 
 mv -v data/clean/country.cc.processed data/clean/country.cc.txt
